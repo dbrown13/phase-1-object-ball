98a84551efeda081c3e9681b21defcca48e13fb8
Deryn Brown <Deryn.2017@gmail.com>
2022-09-13 08:38:47 -0500

    commit tree f40c67a053628ead1c09659948e2f3a676248f1d

---
+++ src/00-objectball.js
@@ -0,0 +1,16 @@
+function gameObject {
+    home = {
+        teamName =  "Brooklyn Nets",
+        colors = ["black", "white"],
+        players = {
+
+        },
+    }
+    away = {
+        teamName = "Charlotte Hornets",
+        colors =  ["Turqoise", "Purple"],
+        players =  {
+
+        },
+    }
+}
